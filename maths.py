eb022fde5d0a4883ced638fc2ec44465fba361be
unknown <wsegurar@gmail.com>
2021-04-28 01:49:11 -0500

    cambio de ruta de action

--- maths.py
+++ maths.py
@@ -1,13 +1,6 @@
-def add_ten(x):
-    if type(x) == str:
-        x_num = int(x)
-        result_num = x_num + 10
-        return str(result_num)
-    elif x is None:
-        return None
-    else:
-        return x + 10
-
 def sumar(x,y):
     return x - y
+
+def hello():
+    return "Hello world"
 
--- tests.py
+++ tests.py
@@ -1,22 +1,12 @@
 """ Tests for maths.py """
 import pytest
 
-from maths import add_ten
+from maths import hello
 from maths import sumar
 
-def test_add_ten():
-    """ Tests the add_ten function """
-    assert 15 == add_ten(5)
-
-
-def test_add_ten_str_int():
-    """ Tests the add_ten function for string input """
-    assert add_ten('5') == '15'
-
-
-def test_add_ten_none():
-    """ Tests that add_ten throws a ValueError when None input given """
-    assert add_ten(None) is None
+def test_hello():
+    """ Test escribir hello world """
+    assert "Hello World" == hello()
 
 def test_sumar_numeros():
     """ Test sumando 2 numeros """
