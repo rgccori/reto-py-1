a010a00b6ebd680e39f921b878242229cd088486
ROCIO GABRIELA CCORI FLOREZ <89796936+rgccori@users.noreply.github.com>
2021-09-04 00:48:51 -0500

    Update tests.py
    corrección de error W por w

--- tests.py
+++ tests.py
@@ -1,11 +1,13 @@
 """ Tests for maths.py """
+
 import pytest
 
 from hello import holaMundo
 
 def test_holaMundo():
     """ Test escribir hello world """
-    assert "Hello World" == holaMundo()
+
+    assert "Hello world" == holaMundo()
 
 
 
